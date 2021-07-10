63141ae8a68ad0a0e4790630893121839b3ac619
WX <azure.cwx@gmail.com>
2021-07-10 17:47:08 +0800

    multiple changes. final edit?

--- List/ContentView.swift
+++ List/ContentView.swift
@@ -9,52 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     // array 
-    @State var friends = [
-        Friend(name: "Wei Xuan", icon: "bolt.horizontal", school: "Presbyterian High", Image: "ws", attack: 90.0, defence: 100.0, replySpeed: 80.0, types: [.normal, .water, .ice]),
-        Friend(name: "TraceTogether", icon: "shield.lefthalf.fill", school: "GovTech", Image: "RickQR", attack: 100.0, defence: 100.0, replySpeed: 100.0, types: [.command]),
-        Friend(name: "Mg 🌊🏌️🦁", icon: "person.fill", school: "Presbyterian High", Image: "ws", attack: 100.0, defence: 100.0, replySpeed: 0.0, types: [.normal, .grass, .fire])]
+    //@State var friends = [
+        //Friend(name: "Wei Xuan", icon: "bolt.horizontal", school: "Presbyterian High", Image: "ws", attack: 90.0, defence: 100.0, replySpeed: 80.0, types: [.normal, .water, .ice]),
+        //Friend(name: "TraceTogether", icon: "shield.lefthalf.fill", school: "GovTech", Image: "RickQR", attack: 100.0, defence: 100.0, replySpeed: 100.0, types: [.command]),
+       // Friend(name: "Mg 🌊🏌️🦁", icon: "person.fill", school: "Presbyterian High", Image: "ws", attack: 100.0, defence: 100.0, replySpeed: 0.0, types: [.normal, .grass, .fire])]
+    
+    @Binding var friends: [Friend]
     
     @State var isSheetPresented = false
     
     
     var body: some View {
         NavigationView {
-            List (0..<friends.count) { index in
-                NavigationLink(destination: VictimDetailView(friend: $friends[index])) {
-                    Image(systemName: friends[index].icon)
+            List {
+                ForEach(friends) { friend in
+                    let friendIndex = friends.firstIndex(of: friend)!
                     
-                    VStack(alignment: .leading) {
-                        Text(friends[index].name)
-                            .bold()
+                    NavigationLink(destination: VictimDetailView(friend: $friends[friendIndex])) {
+                        Image(systemName: friend.icon)
                         
-                        HStack {
-                            Text(friends[index].school)
-                            
-                            Spacer()
-                            
-                            ForEach(friends[index].types, id: \.rawValue) { type in
-                                Image(systemName: type.getSymbolName())
+                        VStack(alignment: .leading) {
+                            Text(friend.name)
+                                .bold()
+                            HStack {
+                                Text(friend.school)
+                                
+                                Spacer()
+                                
+                                ForEach(friend.types, id: \.rawValue) { type in
+                                    Image(systemName: type.getSymbolName())
+                                }
                             }
-
                         }
-                        .foregroundColor(.gray)
                     }
+                }.onDelete { offsets in
+                    friends.remove(atOffsets: offsets)
+                }.onMove { source, destination in
+                    friends.move(fromOffsets: source, toOffset: destination)
                 }
             }
-            .navigationTitle("Innocent Victims >:D")
-            .navigationBarItems(leading: EditButton(), trailing: Button(action: { isSheetPresented = true}, label: {
-                Image(systemName: "plus")
-            }))
+            .navigationTitle("Friends")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                                    isSheetPresented = true
+                                }, label: {
+                                    Image(systemName: "plus")
+                                }))
         }.sheet(isPresented: $isSheetPresented) {
             NewFriendType(friends: $friends)
         }
-
     }
+    
 }
+
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(friends: .constant([Friend(name: "Wei Xuan", icon: "bolt.horizontal", school: "Presbyterian High", Image: "ws", attack: 90.0, defence: 100.0, replySpeed: 80.0, types: [.normal, .water, .ice]), Friend(name: "TraceTogether", icon: "shield.lefthalf.fill", school: "GovTech", Image: "RickQR", attack: 100.0, defence: 100.0, replySpeed: 100.0, types: [.command]), Friend(name: "Mg 🌊🏌️🦁", icon: "person.fill", school: "Presbyterian High", Image: "ws", attack: 100.0, defence: 100.0, replySpeed: 0.0, types: [.normal, .grass, .fire])]))
     }
 }
--- List/FriendsData.swift
+++ List/FriendsData.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import SwiftUI
+
+class FriendsData: ObservableObject {
+    @Published var friends: [Friend] = []
+    
+    
+    let sampleFriends = [Friend(name: "Wei Xuan", icon: "bolt.horizontal", school: "Presbyterian High", Image: "ws", attack: 90.0, defence: 100.0, replySpeed: 80.0, types: [.normal, .water, .ice]),
+        Friend(name: "TraceTogether", icon: "shield.lefthalf.fill", school: "GovTech", Image: "RickQR", attack: 100.0, defence: 100.0, replySpeed: 100.0, types: [.command]),
+        Friend(name: "Mg 🌊🏌️🦁", icon: "person.fill", school: "Presbyterian High", Image: "ws", attack: 100.0, defence: 100.0, replySpeed: 0.0, types: [.normal, .grass, .fire])]
+    
+    
+    func getArchiveURL() -> URL {
+        let plistName = "friends.plist"
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentsDirectory.appendingPathComponent(plistName)
+    }
+    func save() {
+        let archiveURL = getArchiveURL()
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedFriends = try? propertyListEncoder.encode(friends)
+        try? encodedFriends?.write(to: archiveURL, options: .noFileProtection)
+    }
+    
+    func load() {
+        let archiveURL = getArchiveURL()
+        let propertyListDecoder = PropertyListDecoder()
+        
+        var finalFriends: [Friend]!
+        
+        if let retrievedFriendsData = try? Data(contentsOf: archiveURL),
+           let decodedFriends = try? propertyListDecoder.decode(Array<Friend>.self, from: retrievedFriendsData) {
+            finalFriends = decodedFriends
+        } else {
+            finalFriends = sampleFriends
+        }
+        friends = finalFriends
+    }
+}
